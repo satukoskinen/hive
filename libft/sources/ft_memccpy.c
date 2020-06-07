d10822971478f5e96735d141bbe5da7320b8951b
Satu Koskinen <satu.a.koskinen@gmail.com>
2020-06-07 21:38:19 +0300

    updated project libft

---
+++ libft/sources/ft_memccpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skoskine <skoskine@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/07 21:16:16 by skoskine          #+#    #+#             */
+/*   Updated: 2020/06/07 21:35:42 by skoskine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
+{
+	size_t i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (((char*)src)[i] == (unsigned char)c)
+			return (src++);
+		((char*)dst)[i] = ((char*)src)[i];
+		i++;
+	}
+	return (NULL);
+}
